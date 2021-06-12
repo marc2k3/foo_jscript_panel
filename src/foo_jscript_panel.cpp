caefaf08ecb8e5ba3ddb81f071c5839f0e1662b7
marc2k3 <marc2003@live.co.uk>
2021-06-12 21:29:23 +0100

    Remove CAppModule

--- src/foo_jscript_panel.cpp
+++ src/foo_jscript_panel.cpp
@@ -7,22 +7,15 @@ namespace jsp
 	DECLARE_COMPONENT_VERSION(component_name, component_version, component_info);
 	VALIDATE_COMPONENT_FILENAME(component_dll_name);
 
-	CAppModule app;
 	GdiplusScope scope;
 
 	extern "C" BOOL WINAPI DllMain(HINSTANCE ins, DWORD reason, LPVOID)
 	{
 		if (reason == DLL_PROCESS_ATTACH)
 		{
-			app.Init(nullptr, ins);
-
 			std::array<wchar_t, MAX_PATH> path;
 			GetModuleFileName(ins, path.data(), path.size());
 			return SUCCEEDED(LoadTypeLibEx(path.data(), REGKIND_NONE, &g_typelib));
-		}
-		else if (reason == DLL_PROCESS_DETACH)
-		{
-			app.Term();
 		}
 		return TRUE;
 	}
